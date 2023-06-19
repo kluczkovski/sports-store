3a2d04cd6c117784feda97e3bb77ea91a462029e
EvertonKluczkovski <kluczkovski@hotmail.com>
2023-06-19 22:52:12 +0100

    added HomeController Unit Test added HomeView page

--- src/SportsStore.WebApp/Controllers/HomeController.cs
+++ src/SportsStore.WebApp/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SportsStore.WebApp.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,8 +11,14 @@ namespace SportsStore.WebApp.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IStoreRepository _repository;
+
+        public HomeController(IStoreRepository repository)
+        {
+            _repository = repository;
+        }
         // GET: /<controller>/
-        public IActionResult Index() => View();
+        public IActionResult Index() => View(_repository.Products);
     }
 }
 
